8fdcfb715d2b23d48606994d339ac5bcbd3341fd
Cheng Vue <cvu3@ucsc.edu>
2018-12-29 04:09:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -7,4 +7,20 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.where("rating = ?", self.highest_rating)[0]
   end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.where("rating = ?", self.lowest_rating)[0]
+  end
+
+  def self.rating_sum
+    Show.sum(:rating)
+  end
+
+  def
+    Show.where("rating > 5")[0]
+  end
 end
